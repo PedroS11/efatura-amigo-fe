a3e9af54edcc9c5ab0eabf1a67102f481a25a60a
Pedro Oliveira da Silva <9307951+PedroS11@users.noreply.github.com>
2026-09-11 12:46:43 +0100

    Make it work locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
     return {
         plugins: [react(), tailwindcss()],
         resolve: {
@@ -11,17 +13,16 @@ export default defineConfig(() => {
                 "@": path.resolve(__dirname, "./src"),
             },
         },
-        // server: {
-        //     proxy: env.VITE_API_PROXY_TARGET
-        //         ? {
-        //               "/api": {
-        //                   target: env.VITE_API_PROXY_TARGET,
-        //                   changeOrigin: true,
-        //                   secure: true,
-        //                   configure: configureDevProxyCookies,
-        //               },
-        //           }
-        //         : undefined,
-        // },
+        server: {
+            proxy:
+                mode === "development"
+                    ? {
+                          "/api": {
+                              target: env.VITE_API_PROXY_TARGET,
+                              changeOrigin: true,
+                          },
+                      }
+                    : undefined,
+        },
     };
 });
